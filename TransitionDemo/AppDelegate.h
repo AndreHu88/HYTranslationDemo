42a2292f8b9ef9f5f657cc2d524b401bad118f93
leimo <leimo@huyongdeimac.local>
2017-08-07 08:46:31 +0800

    Initial Commit

---
+++ TransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransitionDemo
+//
+//  Created by leimo on 2017/8/7.
+//  Copyright © 2017年 huyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
